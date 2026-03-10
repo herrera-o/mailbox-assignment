76b797d883411b8ccaec948c8929845212d749c6
Oscar Herrera <herrera_o@outlook.com>
2026-03-10 15:57:42 -0700

    Updated class to implement Serializable.

--- src/main/java/dev/herrerao/Message.java
+++ src/main/java/dev/herrerao/Message.java
@@ -1,6 +1,7 @@
 package dev.herrerao;
 
 import java.time.LocalDateTime;
+import java.io.Serializable;
 
 public record Message(int messageID, int senderID, String senderName, String subject, String body,
-                      LocalDateTime sent_at, Boolean is_read) {}
+                      LocalDateTime sent_at, Boolean is_read) implements Serializable {}
