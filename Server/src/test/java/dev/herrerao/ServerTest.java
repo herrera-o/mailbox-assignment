1a37774da41c324c07e80ee8f1b552f9477797b3
Oscar Herrera <herrera_o@outlook.com>
2026-03-17 20:30:15 -0700

    removed test case in server, for now.

--- Server/src/test/java/dev/herrerao/ServerTest.java
+++ Server/src/test/java/dev/herrerao/ServerTest.java
@@ -6,19 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ServerTest {
 
-    @Test
-    public void testServer() {
-
-        try {
-            Thread mainThread = Thread.currentThread();
-            Server srv = new Server(new DataStorage());
-            srv.startServer();
-
-            mainThread.join();
-
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        assertTrue(true);
-    }
+//    @Test
+//    public void testServer() {
+//
+//        try {
+//            Thread mainThread = Thread.currentThread();
+//            Server srv = new Server(new DataStorage());
+//            srv.startServer();
+//
+//            mainThread.join();
+//
+//        } catch (Exception e) {
+//            System.out.println(e.toString());
+//        }
+//        assertTrue(true);
+//    }
 }
